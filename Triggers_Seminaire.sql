f4e933767eced652e22d79224ee344bcec5832df
Thierno Diallo <diallo1@etu.univ-grenoble-alpes.fr>
2018-03-16 12:42:13 +0100

    TRIGGER Nbr de conference par jour

--- Triggers_Seminaire.sql
+++ Triggers_Seminaire.sql
@@ -4,6 +4,62 @@ Create or replace trigger NbrSeminairePossibleParJour
 Before insert on SEMINAIRE
 For each row
 Declare
+	NbSeminaire integer;
+Begin
+	select count(*) into NbSeminaire from SEMINAIRE ;
+	If NbSeminaire > 3 then raise_application_error(-20100,'Impossible d\'organiser plus de 3 seminaire à la fois'); end if;
+end;
+/ 
+--######################### CREATION DU TRIGGER CONFIRMATION RESERVATION SALLE 1 MOIS A L'AVANCE  ###############
+Create or replace trigger NbrSeminairePossibleParJour
+Before insert on SEMINAIRE
+For each row
+Declare
+	NbDisponible integer;
+	NbPlaceReserve integer;
+Begin
+	select nbplacespossibles into NbDisponible from LesRepresentations 
+	where dateRep=:new.dateRep and numS=:new.numS;
+	select sum(nbplaces) into NbPlaceReserve from LesReservations where dateRep=:new.dateRep and numS=:new.numS;
+	If NbDisponible - NbPlaceReserve < :new.nbplaces 
+		then raise_application_error(-20100,'Impossible, Plus de place disponible'); end if;
+end;
+/ 
+--################################### CREATION DU TRIGGER NOMBRE DE SEMINAIRE PAR JOUR  #########################
+Create or replace trigger NbrSeminairePossibleParJour
+Before insert on SEMINAIRE
+For each row
+Declare
+	NbDisponible integer;
+	NbPlaceReserve integer;
+Begin
+	select nbplacespossibles into NbDisponible from LesRepresentations 
+	where dateRep=:new.dateRep and numS=:new.numS;
+	select sum(nbplaces) into NbPlaceReserve from LesReservations where dateRep=:new.dateRep and numS=:new.numS;
+	If NbDisponible - NbPlaceReserve < :new.nbplaces 
+		then raise_application_error(-20100,'Impossible, Plus de place disponible'); end if;
+end;
+/ 
+--################################### CREATION DU TRIGGER NOMBRE DE SEMINAIRE PAR JOUR  #########################
+Create or replace trigger NbrSeminairePossibleParJour
+Before insert on SEMINAIRE
+For each row
+Declare
+	NbDisponible integer;
+	NbPlaceReserve integer;
+Begin
+	select nbplacespossibles into NbDisponible from LesRepresentations 
+	where dateRep=:new.dateRep and numS=:new.numS;
+	select sum(nbplaces) into NbPlaceReserve from LesReservations where dateRep=:new.dateRep and numS=:new.numS;
+	If NbDisponible - NbPlaceReserve < :new.nbplaces 
+		then raise_application_error(-20100,'Impossible, Plus de place disponible'); end if;
+end;
+/ 
+--################################### CREATION DU TRIGGER NOMBRE DE SEMINAIRE PAR JOUR  #########################
+Create or replace trigger NbrSeminairePossibleParJour
+Before insert on SEMINAIRE
+For each row
+Declare
 	NbDisponible integer;
 	NbPlaceReserve integer;
 Begin
