f53c2a397e85dbe0da9001557902dd78f94aba0b
Thierno Ibrahima Sory Diallo <thierno.diallo2@etu.univ-grenoble-alpes.fr>
2018-03-16 11:06:27 +0100

    Create creat.sql

---
+++ creat.sql
@@ -0,0 +1,5 @@
+
+--############################## DROP TABLE IN DATABASE #####################################
+DROP TABLE LesReservations;
+DROP TABLE LesRepresentations;
+DROP TABLE LesSpectacles;
